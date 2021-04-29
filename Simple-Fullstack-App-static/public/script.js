7f47b4b1a2154ada73f5bc4d12c0a0374c989579
NickW629 <56590336+NickW629@users.noreply.github.com>
2021-04-29 13:11:39 -0400

    Add files via upload

---
+++ Simple-Fullstack-App-static/public/script.js
@@ -0,0 +1,118 @@
+const $reviewsContainer = document.getElementById("reviews")
+const $usersContainer = document.getElementById("users")
+document.getElementById("createReview")
+    .onclick = createReview
+
+
+spawnReviews()
+
+//spawnUsers()
+
+let user_id
+
+function createReview(e) {
+    e.preventDefault()
+
+    fetch("/create", {
+        body: JSON.stringify({
+            userId: localStorage.getItem("userId"),
+            videogame: document.getElementById("videogame"),
+            publisher: document.getElementById("publisher"),
+            developer: document.getElementById("developer"),
+            genre: document.getElementById("genre"),
+            rate: document.getElementById("rating"),
+            reason: document.getElementById("reason")
+        }),
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(res => res.json())
+        .then(res => {
+        })
+        .catch(err => console.error(err))
+}
+
+function spawnReviews() {
+   fetch("/reviews")
+    .then(res => res.json())
+    .then(reviews => {
+        const reviewsHTML = reviews.map( review => `
+        <div class="row">
+          <div class="col-sm-3">
+            <div class="well" data-reviewid=${review.id}>
+             <p>${review.user_id}</p>
+             <img src="maleAvatar.PNG" class="img-circle" height="45" width="45" alt="Avatar">
+            </div>
+          </div>
+          <div class="col-sm-9" >
+            <div class="well">
+              <p>
+                Game:${review.videogame}<br>
+                 Publisher: ${review.publisher}<br>
+                 Developer: ${review.developer}<br>
+                 Genre: ${review.genre}<br>
+                 Rating: ${review.rate}/5</br>
+                 Why: ${review.reason}
+              </p>
+              <button type="button" class="btn btn-default btn-sm">
+                <span class="glyphicon glyphicon-fire"></span> Enjoyed game
+                <button type="button" class="btn btn-default btn-sm">
+                    <span class="glyphicon glyphicon-thumbs-down"></span> Did not enjoy game
+            </div>
+          </div>
+        </div>
+        ` ).join("")
+        $reviewsContainer.innerHTML = reviewsHTML
+    })
+    .catch(err => console.error(err))
+   
+}
+
+function spawnUsers() {
+    fetch("/users")
+     .then(res => res.json())
+     .then(users => {
+         const usersHTML = users.map( user => `
+         <div class="user" data-userid=${user.id}>
+             <p>${user.username}</p>
+             <div class="details">
+                 <div>${user.firstName}</div>
+             </div>
+             <button onclick="e => {addFriend(e);}">Add Friend</button>
+         </div>
+         ` ).join("")
+         $usersContainer.innerHTML = usersHTML
+     })
+     .catch(err => console.error(err))
+    
+ }
+
+function addFriend(e) {
+    const $userDiv = e.target.parentElement
+    const friend_id = $userDiv.userid
+
+    const payload = {
+        body: JSON.stringify({
+            user_id: user_id,
+            friend_id: friend_id
+        }),
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    fetch("/friends", payload)
+        .then(res => res.json())
+        .then(res => console.log(res.body))
+        .catch(error => console.error(error))
+}
+
+function loadData() {
+    return {
+        posts: [
+            
+        ]
+    }
+}
---
+++ Simple-Fullstack-App-static/server.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const sqlite3 = require('sqlite3')
+const app = new express()
+const db = new sqlite3.Database('./db/gamepage.db')
+const reviews = loadData().reviews
+
+app.use(express.static('public'))
+app.use(express.json())
+
+app.get("/reviews", (req,res) => {
+    const sql = "SELECT * FROM reviews;"
+    db.all(sql,[],(err, rows) => {
+        res.send(rows)
+    })
+})
+
+app.get("/users", (req,res) => {
+    const sql = "SELECT * FROM users"
+    db.all(sql,[],(err, rows) => {
+        res.send(rows)
+    })
+})
+
+app.post("/friends", (req, res) => {
+    const friendship = req.body
+    const sql = "INSERT INTO users_users (userid1, userid2) VALUES (?,?)"
+    db.run(sql,[friendship.user_id, friendship.friend_id],(err) => {
+        if (err) console.error(err)
+        res.send({
+            message: "You are now friends!",
+            friendshipID: this.lastID
+        })
+    })
+})
+
+app.post("/reviews", (req,res)=> {
+    const review = req.body;
+    if (review.text.length >= 5) {
+        const sql = "INSERT INTO reviews (videogame, publisher, developer, genre, rate, reason, user_id) VALUES (?, ?, ?, ?, ?, ?, ?);"
+        db.run(sql,[review.videogame, review.publisher, review.developer, review.genre, review.rate, review.text, 0])
+        res.send({
+            message: "Review successfully saved"
+        })
+    }
+    else {
+        res.send({
+            message: "Review is not long enough."
+        })
+    }
+})
+
+app.post("/login", (req, res) => {
+    const user = req.body
+    const sql2 = "SELECT id, first_name, last_name FROM users WHERE username = ? AND password = ?"
+    db.all(sql2,[user.username, user.password],(err, rows) => {
+        if (rows && rows.length > 0) {
+            res.send({
+                message: "Successful login!",
+                userId: rows[0].id
+            })
+        }
+    
+    else {
+        if (user.username.length >= 4 && user.password.length >= 4) {
+            const sql = "INSERT INTO users (username, password) VALUES (?,?)"
+            db.run(sql,[user.username, user.password],(err) => {
+                if (err) console.error(err)
+                const idSQL = "SELECT id FROM users WHERE username = ?"
+                db.get(idSQL, [user.username], (err, row) => {
+                    if (err) console.error(err)
+                    if (row) res.send({
+                        message: "Your account was successfully created.",
+                        userId: row.id
+                    })
+                })
+            })
+        }
+        else {
+            res.status(401)
+            res.send({
+                message: "Username or password is invalid."
+            })
+        }
+    }
+    })
+})
+
+app.listen(3000, () => console.log("Server started"))
+
+function loadData() {
+    return {
+        users:
+        [
+            
+        ]
+    }
+}
